5f51bed2a1533bc870d0f744ec07e29a09123b9a
Jose Galvez-Hernandez <josemgalvezh90@gmail.com>
2024-05-19 15:14:56 -0700

    wip: Refactor skeleton files

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
