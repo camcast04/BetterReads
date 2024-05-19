11a550bdeaf6345f1f22b7dbbc6876ce4a527123
Jose Galvez-Hernandez <josemgalvezh90@gmail.com>
2024-05-19 14:55:26 -0700

    wip: Refactor skeleton files

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,9 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
+
 
 export default function App() {
   const [user, setUser] = useState(getUser());
@@ -15,11 +14,8 @@ export default function App() {
       { user ?
           <>
             <NavBar user={user} setUser={setUser} />
-            <Routes>
-              {/* Route components in here */}
-              <Route path="/orders/new" element={<NewOrderPage />} />
-              <Route path="/orders" element={<OrderHistoryPage />} />
-            </Routes>
+            {/* <Routes>
+            </Routes> */}
           </>
           :
           <AuthPage setUser={setUser} />
